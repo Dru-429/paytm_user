f640ad9406d8b47f3bf1034d46e0c872ed573fd1
Dru-429 <ect.dhruv@gmail.com>
2026-05-23 17:37:14 +0530

    Addede Trnsfer route

--- backend/routes/accounts.js
+++ backend/routes/accounts.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const authmiddleware = require("../middelware");
-const { Accounts } = require("../models/db");
+const { Accounts, Users } = require("../models/db");
+const { default: mongoose } = require("mongoose");
 
 const router = express.Router();
 router.use(express.json());
@@ -10,11 +11,65 @@ router.get("/balance", authmiddleware, async (req, res) => {
   const { userId } = parseInt(req.body);
 
   const balanceAccount = await Accounts.findById(userId);
-  const balance = balanceAccount.balance
+  const balance = balanceAccount.balance;
 
   res.status(200).send({
-    balance
+    balance,
   });
+});
+
+//accounts/transfer
+router.put("/transfer", authmiddleware, async (req, res) => {
+  const { to, amount } = req.body;
+  const userId = parseInt(req.body);
+
+  const session = await mongoose.startSessionession();
+
+  const sender = await Accounts.findById(userId).session(session);
+
+  if (sender.balance < amount) {
+    await session.endSession();
+    return res.status(400).json({
+      message: "Insaficent Balance ",
+    });
+  }
+
+  const reciver = await Users.findOne({
+    _id: to,
+  }).session(session);
+
+  if (!reciver) {
+    await session.endSession();
+    return res.status(400).json({
+      message: "Reciver not found",
+    });
+  }
+
+  //performing transaction
+  await Accounts.updateOne(
+    {
+      _id: userId,
+    },
+    {
+      $inc: {
+        balance: -amount,
+      },
+    },
+  ).session(session);
+
+  await Accounts.updateOne({
+    _id: to,
+  }, {
+    $inc: {
+      balance: amount
+    }
+  }).session(session);
+
+  await session.commitTransaction();
+
+  res.status(200).json({
+    message: "Transfer Successfull",
+  })
 });
 
 module.exports = {
